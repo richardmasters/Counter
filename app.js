955ff2b26eebb6d20c5622be69ae19a198efed6e
richardmasters <richardmasters164@gmail.com>
2022-09-16 12:33:41 -0400

    Finished
    javascript rewritten to work

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-let count = 0
+let count = 0;
 const value = document.querySelector("#value");
 const buttons = document.querySelector(".hi");
 
-buttons.forEach(function (hello) {
-    hello.addEventListener("click", function (e) {
-        const styles = e.currentTarget.classList;
-        if (styles.contains("#decrease")) {
+buttons.addEventListener("click", function (e) {
+    if (e.target !== this) {
+        const styles = e.target.classList;
+        if (styles.contains('decrease')) {
             count--
-        } else if (styles.contains("#increase")) {
+        } else if (styles.contains('increase')) {
             count++
         } else {
             count = 0;
         }
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 0) {
+            value.style.color = "black";
+        }
 
         value.textContent = count;
-    });
-});
+    }
+})
